2c8bd34e6cb5a3d8d0a81eb403943e4096c30d1b
LAKSHA SAINI <lakshaysaini7827@gmail.com>
2024-08-26 21:22:04 +0530

    initial stage 0

---
+++ port.js
@@ -0,0 +1,5 @@
+type="text/javascript" src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js">
+var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+  });
